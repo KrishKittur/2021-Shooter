8cb09bf24e8ed37ec1e4c7ef16036855f8a9869f
KrishKittur <73321517+KrishKittur@users.noreply.github.com>
2021-02-25 12:06:32 -0800

    Add some subsystem templates

---
+++ src/main/java/frc/robot/subsystems/shooter/AcceleratorSubsystem.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Accelerator Subsystem
+public class AcceleratorSubsystem extends SubsystemBase {
+    
+}
